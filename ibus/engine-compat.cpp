afdccd6e202b5b9bc5af8f3c50b2d4acf9887f70
Jerin Philip <jphilip@ed.ac.uk>
2021-10-14 16:33:18 +0100

    Changing bergamot->lemonade

--- ibus/engine-compat.cpp
+++ ibus/engine-compat.cpp
@@ -1,93 +1,93 @@
 #include "engine-compat.h"
 
-typedef struct _IBusBergamotEngine IBusBergamotEngine;
-typedef struct _IBusBergamotEngineClass IBusBergamotEngineClass;
+typedef struct _IBusLemonadeEngine IBusLemonadeEngine;
+typedef struct _IBusLemonadeEngineClass IBusLemonadeEngineClass;
 
-struct _IBusBergamotEngine {
+struct _IBusLemonadeEngine {
   IBusEngine parent;
   GString *preedit;
   gint cursor_pos;
   IBusLookupTable *table;
 };
 
-struct _IBusBergamotEngineClass {
+struct _IBusLemonadeEngineClass {
   IBusEngineClass parent;
 };
 
 /* functions prototype */
-static void ibus_bergamot_engine_class_init(IBusBergamotEngineClass *klass);
-static void ibus_bergamot_engine_init(IBusBergamotEngine *engine);
-static void ibus_bergamot_engine_destroy(IBusBergamotEngine *engine);
-static gboolean ibus_bergamot_engine_process_key_event(IBusEngine *engine,
+static void ibus_lemonade_engine_class_init(IBusLemonadeEngineClass *klass);
+static void ibus_lemonade_engine_init(IBusLemonadeEngine *engine);
+static void ibus_lemonade_engine_destroy(IBusLemonadeEngine *engine);
+static gboolean ibus_lemonade_engine_process_key_event(IBusEngine *engine,
                                                        guint keyval,
                                                        guint keycode,
                                                        guint modifiers);
-static void ibus_bergamot_engine_focus_in(IBusEngine *engine);
-static void ibus_bergamot_engine_focus_out(IBusEngine *engine);
-static void ibus_bergamot_engine_reset(IBusEngine *engine);
-static void ibus_bergamot_engine_enable(IBusEngine *engine);
-static void ibus_bergamot_engine_disable(IBusEngine *engine);
+static void ibus_lemonade_engine_focus_in(IBusEngine *engine);
+static void ibus_lemonade_engine_focus_out(IBusEngine *engine);
+static void ibus_lemonade_engine_reset(IBusEngine *engine);
+static void ibus_lemonade_engine_enable(IBusEngine *engine);
+static void ibus_lemonade_engine_disable(IBusEngine *engine);
 static void ibus_engine_set_cursor_location(IBusEngine *engine, gint x, gint y,
                                             gint w, gint h);
-static void ibus_bergamot_engine_set_capabilities(IBusEngine *engine,
+static void ibus_lemonade_engine_set_capabilities(IBusEngine *engine,
                                                   guint caps);
-static void ibus_bergamot_engine_page_up(IBusEngine *engine);
-static void ibus_bergamot_engine_page_down(IBusEngine *engine);
-static void ibus_bergamot_engine_cursor_up(IBusEngine *engine);
-static void ibus_bergamot_engine_cursor_down(IBusEngine *engine);
-static void ibus_bergamot_property_activate(IBusEngine *engine,
+static void ibus_lemonade_engine_page_up(IBusEngine *engine);
+static void ibus_lemonade_engine_page_down(IBusEngine *engine);
+static void ibus_lemonade_engine_cursor_up(IBusEngine *engine);
+static void ibus_lemonade_engine_cursor_down(IBusEngine *engine);
+static void ibus_lemonade_property_activate(IBusEngine *engine,
                                             const gchar *prop_name,
                                             gint prop_state);
-static void ibus_bergamot_engine_property_show(IBusEngine *engine,
+static void ibus_lemonade_engine_property_show(IBusEngine *engine,
                                                const gchar *prop_name);
-static void ibus_bergamot_engine_property_hide(IBusEngine *engine,
+static void ibus_lemonade_engine_property_hide(IBusEngine *engine,
                                                const gchar *prop_name);
 
-static void ibus_bergamot_engine_commit_string(IBusBergamotEngine *bergamot,
+static void ibus_lemonade_engine_commit_string(IBusLemonadeEngine *lemonade,
                                                const gchar *string);
-static void ibus_bergamot_engine_update(IBusBergamotEngine *bergamot);
+static void ibus_lemonade_engine_update(IBusLemonadeEngine *lemonade);
 
-G_DEFINE_TYPE(IBusBergamotEngine, ibus_bergamot_engine, IBUS_TYPE_ENGINE)
+G_DEFINE_TYPE(IBusLemonadeEngine, ibus_lemonade_engine, IBUS_TYPE_ENGINE)
 
-static void ibus_bergamot_engine_class_init(IBusBergamotEngineClass *klass) {
+static void ibus_lemonade_engine_class_init(IBusLemonadeEngineClass *klass) {
   IBusObjectClass *ibus_object_class = IBUS_OBJECT_CLASS(klass);
   IBusEngineClass *engine_class = IBUS_ENGINE_CLASS(klass);
 
   ibus_object_class->destroy =
-      (IBusObjectDestroyFunc)ibus_bergamot_engine_destroy;
+      (IBusObjectDestroyFunc)ibus_lemonade_engine_destroy;
 
-  engine_class->process_key_event = ibus_bergamot_engine_process_key_event;
+  engine_class->process_key_event = ibus_lemonade_engine_process_key_event;
 }
 
-static void ibus_bergamot_engine_init(IBusBergamotEngine *bergamot) {
-  bergamot->preedit = g_string_new("");
-  bergamot->cursor_pos = 0;
+static void ibus_lemonade_engine_init(IBusLemonadeEngine *lemonade) {
+  lemonade->preedit = g_string_new("");
+  lemonade->cursor_pos = 0;
 
-  bergamot->table = ibus_lookup_table_new(9, 0, TRUE, TRUE);
-  g_object_ref_sink(bergamot->table);
+  lemonade->table = ibus_lookup_table_new(9, 0, TRUE, TRUE);
+  g_object_ref_sink(lemonade->table);
 }
 
-static void ibus_bergamot_engine_destroy(IBusBergamotEngine *bergamot) {
-  if (bergamot->preedit) {
-    g_string_free(bergamot->preedit, TRUE);
-    bergamot->preedit = NULL;
+static void ibus_lemonade_engine_destroy(IBusLemonadeEngine *lemonade) {
+  if (lemonade->preedit) {
+    g_string_free(lemonade->preedit, TRUE);
+    lemonade->preedit = NULL;
   }
 
-  if (bergamot->table) {
-    g_object_unref(bergamot->table);
-    bergamot->table = NULL;
+  if (lemonade->table) {
+    g_object_unref(lemonade->table);
+    lemonade->table = NULL;
   }
 
-  ((IBusObjectClass *)ibus_bergamot_engine_parent_class)
-      ->destroy((IBusObject *)bergamot);
+  ((IBusObjectClass *)ibus_lemonade_engine_parent_class)
+      ->destroy((IBusObject *)lemonade);
 }
 
-static gboolean ibus_bergamot_engine_process_key_event(IBusEngine *engine,
+static gboolean ibus_lemonade_engine_process_key_event(IBusEngine *engine,
                                                        guint keyval,
                                                        guint keycode,
                                                        guint modifiers) {
   IBusText *text;
-  IBusBergamotEngine *bergamot = (IBusBergamotEngine *)engine;
+  IBusLemonadeEngine *lemonade = (IBusLemonadeEngine *)engine;
 
   if (modifiers & IBUS_RELEASE_MASK)
     return FALSE;
@@ -95,12 +95,12 @@ static gboolean ibus_bergamot_engine_process_key_event(IBusEngine *engine,
   modifiers &= (IBUS_CONTROL_MASK | IBUS_MOD1_MASK);
 
   if (modifiers == IBUS_CONTROL_MASK && keyval == IBUS_s) {
-    // ibus_bergamot_engine_update_lookup_table(bergamot);
+    // ibus_lemonade_engine_update_lookup_table(lemonade);
     return TRUE;
   }
 
   if (modifiers != 0) {
-    if (bergamot->preedit->len == 0)
+    if (lemonade->preedit->len == 0)
       return FALSE;
     else
       return TRUE;
@@ -108,85 +108,85 @@ static gboolean ibus_bergamot_engine_process_key_event(IBusEngine *engine,
 
   switch (keyval) {
   case IBUS_space:
-    g_string_append(bergamot->preedit, " ");
-    // return ibus_bergamot_engine_commit_preedit(bergamot);
+    g_string_append(lemonade->preedit, " ");
+    // return ibus_lemonade_engine_commit_preedit(lemonade);
     return TRUE;
   case IBUS_Return:
-    // return ibus_bergamot_engine_commit_preedit(bergamot);
+    // return ibus_lemonade_engine_commit_preedit(lemonade);
     return TRUE;
 
   case IBUS_Escape:
-    if (bergamot->preedit->len == 0)
+    if (lemonade->preedit->len == 0)
       return FALSE;
 
-    g_string_assign(bergamot->preedit, "");
-    bergamot->cursor_pos = 0;
-    ibus_bergamot_engine_update(bergamot);
+    g_string_assign(lemonade->preedit, "");
+    lemonade->cursor_pos = 0;
+    ibus_lemonade_engine_update(lemonade);
     return TRUE;
 
   case IBUS_Left:
-    if (bergamot->preedit->len == 0)
+    if (lemonade->preedit->len == 0)
       return FALSE;
-    if (bergamot->cursor_pos > 0) {
-      bergamot->cursor_pos--;
-      ibus_bergamot_engine_update(bergamot);
+    if (lemonade->cursor_pos > 0) {
+      lemonade->cursor_pos--;
+      ibus_lemonade_engine_update(lemonade);
     }
     return TRUE;
 
   case IBUS_Right:
-    if (bergamot->preedit->len == 0)
+    if (lemonade->preedit->len == 0)
       return FALSE;
-    if (bergamot->cursor_pos < bergamot->preedit->len) {
-      bergamot->cursor_pos++;
-      ibus_bergamot_engine_update(bergamot);
+    if (lemonade->cursor_pos < lemonade->preedit->len) {
+      lemonade->cursor_pos++;
+      ibus_lemonade_engine_update(lemonade);
     }
     return TRUE;
 
   case IBUS_Up:
-    if (bergamot->preedit->len == 0)
+    if (lemonade->preedit->len == 0)
       return FALSE;
-    if (bergamot->cursor_pos != 0) {
-      bergamot->cursor_pos = 0;
-      ibus_bergamot_engine_update(bergamot);
+    if (lemonade->cursor_pos != 0) {
+      lemonade->cursor_pos = 0;
+      ibus_lemonade_engine_update(lemonade);
     }
     return TRUE;
 
   case IBUS_Down:
-    if (bergamot->preedit->len == 0)
+    if (lemonade->preedit->len == 0)
       return FALSE;
 
-    if (bergamot->cursor_pos != bergamot->preedit->len) {
-      bergamot->cursor_pos = bergamot->preedit->len;
-      ibus_bergamot_engine_update(bergamot);
+    if (lemonade->cursor_pos != lemonade->preedit->len) {
+      lemonade->cursor_pos = lemonade->preedit->len;
+      ibus_lemonade_engine_update(lemonade);
     }
 
     return TRUE;
 
   case IBUS_BackSpace:
-    if (bergamot->preedit->len == 0)
+    if (lemonade->preedit->len == 0)
       return FALSE;
-    if (bergamot->cursor_pos > 0) {
-      bergamot->cursor_pos--;
-      g_string_erase(bergamot->preedit, bergamot->cursor_pos, 1);
-      ibus_bergamot_engine_update(bergamot);
+    if (lemonade->cursor_pos > 0) {
+      lemonade->cursor_pos--;
+      g_string_erase(lemonade->preedit, lemonade->cursor_pos, 1);
+      ibus_lemonade_engine_update(lemonade);
     }
     return TRUE;
 
   case IBUS_Delete:
-    if (bergamot->preedit->len == 0)
+    if (lemonade->preedit->len == 0)
       return FALSE;
-    if (bergamot->cursor_pos < bergamot->preedit->len) {
-      g_string_erase(bergamot->preedit, bergamot->cursor_pos, 1);
-      ibus_bergamot_engine_update(bergamot);
+    if (lemonade->cursor_pos < lemonade->preedit->len) {
+      g_string_erase(lemonade->preedit, lemonade->cursor_pos, 1);
+      ibus_lemonade_engine_update(lemonade);
     }
     return TRUE;
   }
 
   if (is_alpha(keyval)) {
-    g_string_insert_c(bergamot->preedit, bergamot->cursor_pos, keyval);
+    g_string_insert_c(lemonade->preedit, lemonade->cursor_pos, keyval);
 
-    bergamot->cursor_pos++;
-    ibus_bergamot_engine_update(bergamot);
+    lemonade->cursor_pos++;
+    ibus_lemonade_engine_update(lemonade);
 
     return TRUE;
   }
@@ -194,4 +194,4 @@ static gboolean ibus_bergamot_engine_process_key_event(IBusEngine *engine,
   return FALSE;
 }
 
-static void ibus_bergamot_engine_update(IBusBergamotEngine *bergamot) {}
+static void ibus_lemonade_engine_update(IBusLemonadeEngine *lemonade) {}
--- ibus/engine-compat.h
+++ ibus/engine-compat.h
@@ -1,7 +1,7 @@
 #include <ibus.h>
 
-#define IBUS_TYPE_BERGAMOT_ENGINE (ibus_bergamot_engine_get_type())
+#define IBUS_TYPE_LEMONADE_ENGINE (ibus_lemonade_engine_get_type())
 #define is_alpha(c)                                                            \
   (((c) >= IBUS_a && (c) <= IBUS_z) || ((c) >= IBUS_A && (c) <= IBUS_Z))
 
-GType ibus_bergamot_engine_get_type(void);
+GType ibus_lemonade_engine_get_type(void);
